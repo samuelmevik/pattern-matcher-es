9563ea66125b4ec7b3b2c3692b85ae73dfc6e2da
samuelmevik <samuel.mevik@outlook.com>
2022-09-18 14:21:26 +0200

    Ran lint

--- src/guards.ts
+++ src/guards.ts
@@ -1,95 +1,160 @@
-import { Constructor } from "./typings"
+import { Constructor } from './typings'
 
-function typeOf<Type>(type : string) {
-    return function (param : Type) : param is Type {
-        return typeof param === type
-    }
+/**
+ *
+ * @param type
+ */
+function typeOf<Type> (type : string) {
+  return function (param : Type) : param is Type {
+    return typeof param === type
+  }
 }
 
-function instanceOf<Type>(type :  Constructor<Type>) {
-    return function (param : any) : param is Type {
-        return param instanceof type
-    }
+/**
+ *
+ * @param type
+ */
+function instanceOf<Type> (type : Constructor<Type>) {
+  return function (param : any) : param is Type {
+    return param instanceof type
+  }
 }
 
-function isTruthy(param : any) {
-    return Boolean(param)
+/**
+ *
+ * @param param
+ */
+function isTruthy (param : any) {
+  return Boolean(param)
 }
 
-function isFalsy(param : any) {
-    return !isTruthy(param)
+/**
+ *
+ * @param param
+ */
+function isFalsy (param : any) {
+  return !isTruthy(param)
 }
 
-function isNumber(param : any) : param is number {
-    return typeOf<number>('number')(param) && !isNaN(param)
+/**
+ *
+ * @param param
+ */
+function isNumber (param : any) : param is number {
+  return typeOf<number>('number')(param) && !isNaN(param)
 }
 
-function isString(param : any) : param is string {
-    return typeOf('string')(param)
+/**
+ *
+ * @param param
+ */
+function isString (param : any) : param is string {
+  return typeOf('string')(param)
 }
 
-function isBoolean(param : any) : param is boolean {
-    return typeOf('boolean')(param)
+/**
+ *
+ * @param param
+ */
+function isBoolean (param : any) : param is boolean {
+  return typeOf('boolean')(param)
 }
 
-function isUndefined(param : any) : param is undefined {
-    return typeOf('undefined')(param)
+/**
+ *
+ * @param param
+ */
+function isUndefined (param : any) : param is undefined {
+  return typeOf('undefined')(param)
 }
 
-function isDefined(param : any) : param is any {
-    return isFalsy(isUndefined(param))
+/**
+ *
+ * @param param
+ */
+function isDefined (param : any) : param is any {
+  return isFalsy(isUndefined(param))
 }
 
-function isNull(param : any) : param is null {
-    return param === null
+/**
+ *
+ * @param param
+ */
+function isNull (param : any) : param is null {
+  return param === null
 }
 
-function inRange(x : number, y : number) {
-    return function (params: any) {
-        return isNumber(params) && (params >= Math.min(x, y) && params <= Math.max(x, y))
-    }
+/**
+ *
+ * @param x
+ * @param y
+ */
+function inRange (x : number, y : number) {
+  return function (params: any) {
+    return isNumber(params) && (params >= Math.min(x, y) && params <= Math.max(x, y))
+  }
 }
 
+/**
+ *
+ * @param param
+ */
 function isFiniteNumber (param : any) : param is number {
-    return isNumber(param) && !isNaN(param) && isFinite(param)
+  return isNumber(param) && !isNaN(param) && isFinite(param)
 }
 
-function isDate<Type extends RegExp>(param : any) : param is Type {
-    return instanceOf(RegExp)(param)
+/**
+ *
+ * @param param
+ */
+function isDate<Type extends RegExp> (param : any) : param is Type {
+  return instanceOf(RegExp)(param)
 }
 
-function isObject<Type extends Object>(param : any) : param is Type {
-    return isFalsy(isNull(param)) && typeOf('object')(param) && isFalsy(instanceOf(Array)(param))
+/**
+ *
+ * @param param
+ */
+function isObject<Type extends Object> (param : any) : param is Type {
+  return isFalsy(isNull(param)) && typeOf('object')(param) && isFalsy(instanceOf(Array)(param))
 }
 
-function isFunction<Type extends Function>(param : any) : param is Type {
-    return typeOf('function')(param)
+/**
+ *
+ * @param param
+ */
+function isFunction<Type extends Function> (param : any) : param is Type {
+  return typeOf('function')(param)
 }
 
-function is(param : any) {
-    return function (params: number | string) {
-        return param === params
-    }
+/**
+ *
+ * @param param
+ */
+function is (param : any) {
+  return function (params: number | string) {
+    return param === params
+  }
 }
 
 const { isArray } = Array
 
 export {
-    typeOf,
-    isBoolean,
-    isDefined,
-    isFalsy,
-    isNumber,
-    isString,
-    isTruthy,
-    isUndefined,
-    inRange,
-    isDate,
-    isFiniteNumber,
-    instanceOf,
-    isNull,
-    isObject,
-    isFunction,
-    isArray,
-    is
+  typeOf,
+  isBoolean,
+  isDefined,
+  isFalsy,
+  isNumber,
+  isString,
+  isTruthy,
+  isUndefined,
+  inRange,
+  isDate,
+  isFiniteNumber,
+  instanceOf,
+  isNull,
+  isObject,
+  isFunction,
+  isArray,
+  is
 }
--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,87 @@
-import { is, isArray, isFunction, isObject, isString } from "./guards.js";
-import { Cases, Condition, ConditionFunction, ConditionObject } from "./typings";
+import { is, isArray, isFunction, isObject, isString } from './guards.js'
+import { Cases, Condition, ConditionFunction, ConditionObject } from './typings'
 
 const { keys } = Object
 
-function patternMatch(param: any) {
-    return function (...cases: Cases[]) {
-        return cases.find(({ condition }) => checkCondition(condition, param))?.callback()
-    }
+/**
+ *
+ * @param param
+ */
+function patternMatch (param: any) {
+  return function (...cases: Cases[]) {
+    return cases.find(({ condition }) => checkCondition(condition, param))?.callback()
+  }
 }
 
-function checkCondition(condition: Condition, param: any) : boolean {
-    if (isObject<ConditionObject>(condition)) {
-        return keys(condition).every(key => checkCondition(condition[key], param[key]))
-    }
-    else if (isFunction<ConditionFunction>(condition)) {
-        return condition(param)
-    }
-    return false
+/**
+ *
+ * @param condition
+ * @param param
+ */
+function checkCondition (condition: Condition, param: any) : boolean {
+  if (isObject<ConditionObject>(condition)) {
+    return keys(condition).every(key => checkCondition(condition[key], param[key]))
+  } else if (isFunction<ConditionFunction>(condition)) {
+    return condition(param)
+  }
+  return false
 }
 
-
+/**
+ *
+ * @param condition
+ * @param callback
+ */
 function when (condition: Condition, callback: Function) {
-    return ({
-        condition,
-        callback
-    })
+  return ({
+    condition,
+    callback
+  })
 }
 
+/**
+ *
+ * @param callback
+ */
 function otherWise (callback: Function) {
-    return when(() => true, callback)
+  return when(() => true, callback)
 }
 
-function allOf(...conditions : Condition[]) : ConditionFunction {
-    return function (args : any) {
-        return isArray(args) && conditions.every(condition => args.every(arg => checkCondition(condition, arg)))
-    }
+/**
+ *
+ * @param {...any} conditions
+ */
+function allOf (...conditions : Condition[]) : ConditionFunction {
+  return function (args : any) {
+    return isArray(args) && conditions.every(condition => args.every(arg => checkCondition(condition, arg)))
+  }
 }
 
-function anyOf(...conditions : Condition[]) : ConditionFunction {
-    return function (args : any) {
-        return isArray(args) && conditions.some(condition => args.some(arg => checkCondition(condition, arg)))
-    }
+/**
+ *
+ * @param {...any} conditions
+ */
+function anyOf (...conditions : Condition[]) : ConditionFunction {
+  return function (args : any) {
+    return isArray(args) && conditions.some(condition => args.some(arg => checkCondition(condition, arg)))
+  }
 }
 
-function includes(param : any) : ConditionFunction {
-    return function (args: any) {
-        return isArray(args) && args.includes(param)
-    }
+/**
+ *
+ * @param param
+ */
+function includes (param : any) : ConditionFunction {
+  return function (args: any) {
+    return isArray(args) && args.includes(param)
+  }
 }
 
 export {
-    patternMatch,
-    when,
-    otherWise,
-    allOf,
-    anyOf,
-    includes
+  patternMatch,
+  when,
+  otherWise,
+  allOf,
+  anyOf,
+  includes
 }
-
-
