2d82da1d86dbea25a1d199be57175663f584c1c2
samuelmevik <samuel.mevik@outlook.com>
2022-09-18 14:03:38 +0200

    Added types

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,1 +1,8 @@
 export type Constructor<C> = new (...args: any[]) => C
+
+export type ConditionFunction = (...arg: any) => boolean
+export type ConditionObject = { [key: string]: ConditionFunction | ConditionObject }
+
+export type Condition = ConditionFunction | ConditionObject
+
+export type Cases = ({ condition: Condition, callback: Function })
