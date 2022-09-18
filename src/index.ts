45af8a2e79af8045396d3f34622e839f2d2a6f26
samuelmevik <samuel.mevik@outlook.com>
2022-09-18 14:03:59 +0200

    Created index file with mai func

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello world')
+import { is, isArray, isFunction, isObject, isString } from "./guards.js";
+import { Cases, Condition, ConditionFunction, ConditionObject } from "./typings";
+
+const { keys } = Object
+
+function patternMatch(param: any) {
+    return function (...cases: Cases[]) {
+        return cases.find(({ condition }) => checkCondition(condition, param))?.callback()
+    }
+}
+
+
+
+function when (condition: Condition, callback: Function) {
+    return ({
+        condition,
+        callback
+    })
+}
+
+
