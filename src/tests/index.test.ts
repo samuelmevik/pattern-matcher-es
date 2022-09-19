048fa433574624690b1e85efc704267b1e4872f7
samuelmevik <samuel.mevik@outlook.com>
2022-09-19 13:09:45 +0200

    Linted

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -2,8 +2,7 @@ import assert from 'assert'
 import { typeOf } from '../guards.js'
 
 try {
-    assert(typeOf('number')('la'), 'lalal')
+  assert(typeOf('number')('la'), 'lalal')
 } catch (error : AssertionError) {
-    console.log({error});
-    
+  console.log({ error })
 }
