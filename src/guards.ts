7840acd8b38d8f66a4dea6ae4c16dfec31cc6db9
samuelmevik <samuel.mevik@outlook.com>
2022-09-19 13:04:28 +0200

    Added comments.

--- src/guards.ts
+++ src/guards.ts
@@ -1,18 +1,21 @@
 import { Constructor } from './typings'
 
 /**
+ * Checks the type of the operand's value is equals to param's.
  *
- * @param type
+ * @param type Should be a to string literal.
  */
 function typeOf<Type> (type : string) {
   return function (param : Type) : param is Type {
+    // eslint-disable-next-line valid-typeof
     return typeof param === type
   }
 }
 
 /**
+ * Checks if the param is an instance of the class type passed.
  *
- * @param type
+ * @param type The constructor of a class.
  */
 function instanceOf<Type> (type : Constructor<Type>) {
   return function (param : any) : param is Type {
@@ -21,73 +24,82 @@ function instanceOf<Type> (type : Constructor<Type>) {
 }
 
 /**
+ * Turns a value into its boolean equivalant.
  *
- * @param param
+ * @param param The unknown value.
  */
 function isTruthy (param : any) {
   return Boolean(param)
 }
 
 /**
+ * Turns a value into its boolean equivalant and return true if it is false.
  *
- * @param param
+ * @param param The unknown value.
  */
 function isFalsy (param : any) {
   return !isTruthy(param)
 }
 
 /**
+ * Checks if the input is a number.
  *
- * @param param
+ * @param param The unknown value.
  */
 function isNumber (param : any) : param is number {
   return typeOf<number>('number')(param) && !isNaN(param)
 }
 
 /**
+ * Checks if the input is a string.
  *
- * @param param
+ * @param param The unknown value.
  */
 function isString (param : any) : param is string {
   return typeOf('string')(param)
 }
 
 /**
+ * Checks if the input is a boolean.
  *
- * @param param
+ * @param param The unkown value.
  */
 function isBoolean (param : any) : param is boolean {
   return typeOf('boolean')(param)
 }
 
 /**
+ * Checks if the input is undefined.
  *
- * @param param
+ * @param param The unknown value.
  */
 function isUndefined (param : any) : param is undefined {
   return typeOf('undefined')(param)
 }
 
 /**
+ * Checks if the input is defined.
  *
- * @param param
+ * @param param The unknown value.
  */
 function isDefined (param : any) : param is any {
   return isFalsy(isUndefined(param))
 }
 
 /**
+ * Checks if the input is null.
  *
- * @param param
+ * @param param The unkownn value.
  */
 function isNull (param : any) : param is null {
   return param === null
 }
 
 /**
+ * Checks if a value is inbetween parameter x and y. Order of x and y deosnt matter.
  *
- * @param x
- * @param y
+ * @param x The first value in the range.
+ * @param y The second value in the range.
  */
 function inRange (x : number, y : number) {
   return function (params: any) {
@@ -96,8 +108,9 @@ function inRange (x : number, y : number) {
 }
 
 /**
+ * Checks if the input is a finite number.
  *
- * @param param
+ * @param param The unknown value.
  */
 function isFiniteNumber (param : any) : param is number {
   return isNumber(param) && !isNaN(param) && isFinite(param)
@@ -112,24 +125,29 @@ function isDate<Type extends RegExp> (param : any) : param is Type {
 }
 
 /**
+ * Checks if the input is an object.
  *
- * @param param
+ * @param param The unknown value.
  */
+// eslint-disable-next-line @typescript-eslint/ban-types
 function isObject<Type extends Object> (param : any) : param is Type {
   return isFalsy(isNull(param)) && typeOf('object')(param) && isFalsy(instanceOf(Array)(param))
 }
 
 /**
+ * Checks if the input is a function.
  *
- * @param param
+ * @param param The unknown value.
  */
+// eslint-disable-next-line @typescript-eslint/ban-types
 function isFunction<Type extends Function> (param : any) : param is Type {
   return typeOf('function')(param)
 }
 
 /**
+ * Checks if the first parameter is equivalant to the second.
  *
- * @param param
+ * @param param The first parameter.
  */
 function is (param : any) {
   return function (params: number | string) {
